a25a19ef2e0c35c2d1e409850639e384d17e30d0
shiryukuroda <200447028@ccmailg.meijo-u.ac.jp>
2023-07-14 15:12:22 +0900

    first commit

---
+++ include/type_changer.hpp
@@ -0,0 +1,7 @@
+class TypeChanger : public rclcpp::Node{
+  public:
+    TypeChanger(const rclcpp::NodeOption& node_options = rclcpp::node_options());
+    ~Typechanger();
+  private:
+
+};
---
+++ src/type_changer.cpp
@@ -0,0 +1,9 @@
+#include <type_changer.hpp>
+
+TypeChanger::TypeChanger(const rclcpp::NodeOption& node_options):Node("type_changer",node_options){
+  
+}
+
+
+
+
