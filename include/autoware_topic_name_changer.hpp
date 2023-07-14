172f298900a207cb79d950c7f2ac98fa50b001f9
shiryukuroda <200447028@ccmailg.meijo-u.ac.jp>
2023-07-14 19:32:52 +0900

    fix

--- include/autoware_topic_name_changer.hpp
+++ include/autoware_topic_name_changer.hpp
@@ -9,26 +9,26 @@
 class TopicNameChanger : public rclcpp::Node {
   struct Params {
     // rosbag information
-    std::string lidar_topic_name, imu_topic_name, pub_lidar_topic_name, pub_imu_topic_name;
+    std::string lidar_topic_name, imu_topic_name, gnss_topic_name, pub_lidar_topic_name, pub_imu_topic_name, pub_gnss_topic_name;
   } params_;
 
 public:
-  Online(const rclcpp::NodeOptions& node_options = rclcpp::NodeOptions());
-  ~Online();
+  TopicNameChanger(const rclcpp::NodeOptions& node_options = rclcpp::NodeOptions());
+  ~TopicNameChanger();
 
 private:
   // callback
-  void velodyne_callback(const velodyne_msgs::msg::VelodyneScan::SharedPtr msg);
+  void velodyne_points_callback(const velodyne_msgs::msg::VelodynePacket::SharedPtr msg);
 
   void imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg);
 
   void gnss_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg);
 
-  rclcpp::Subscription<velodyne_msgs::msg::VelodyneScan>::SharedPtr velodyne_points_sub_;
+  rclcpp::Subscription<velodyne_msgs::msg::VelodynePacket>::SharedPtr velodyne_points_sub_;
   rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
   rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gnss_sub_;
 
-  rclcpp::Publisher<velodyne_msgs::msg::VelodyneScan>::SharedPtr velodyne_points_pub_;
+  rclcpp::Publisher<velodyne_msgs::msg::VelodynePacket>::SharedPtr velodyne_points_pub_;
   rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
   rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr gnss_pub_;
 };
--- src/autoware_topic_name_changer.cpp
+++ src/autoware_topic_name_changer.cpp
@@ -1,6 +1,6 @@
 #include <autoware_topic_name_changer.hpp>
 
-TopicNameChanger::TopicNameChanger(const rclcpp::NodeOption& node_options):Node("autoware_topic_name_changer",node_options){
+TopicNameChanger::TopicNameChanger(const rclcpp::NodeOptions& node_options) : Node("autoware_topic_name_changer", node_options) {
   params_.lidar_topic_name = this->declare_parameter<std::string>("lidar_topic_name");
   params_.imu_topic_name = this->declare_parameter<std::string>("imu_topic_name");
   params_.gnss_topic_name = this->declare_parameter<std::string>("gnss_topic_name");
@@ -21,25 +21,25 @@ TopicNameChanger::TopicNameChanger(const rclcpp::NodeOption& node_options):Node(
 
   // sub
   auto qos = rclcpp::SensorDataQoS();
-  qos.get_rmw_qos_profile().depteh = 100;
-  velodyne_points_sub_ = this->create_subscription<velodyne_msgs::msg::VelodyneScan>(params_.lidar_topic_name, qos, std::bind(&TopicNameChanger::velodyne_callback, this, std::placeholders::_1));
+  qos.get_rmw_qos_profile().depth = 100;
+  velodyne_points_sub_ = this->create_subscription<velodyne_msgs::msg::VelodynePacket>(
+    params_.lidar_topic_name,
+    qos,
+    std::bind(&TopicNameChanger::velodyne_points_callback, this, std::placeholders::_1));
   imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(params_.imu_topic_name, qos, std::bind(&TopicNameChanger::imu_callback, this, std::placeholders::_1));
   gnss_sub_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(params_.gnss_topic_name, qos, std::bind(&TopicNameChanger::gnss_callback, this, std::placeholders::_1));
 
-  velodyne_points_pub_ = this->create_publisher<velodyne_msgs::msg::VelodyneScan>( params_.lidar_topic_name, 10);
-  imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>( params_.imu_topic_name, 10);
+  velodyne_points_pub_ = this->create_publisher<velodyne_msgs::msg::VelodynePacket>(params_.pub_lidar_topic_name, 10);
+  imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(params_.pub_imu_topic_name, 10);
   gnss_pub_ = this->create_publisher<sensor_msgs::msg::NavSatFix>( params_.pub_gnss_topic_name, 10);
 }
 
-  void TopicNameChanger::velodyne_points_callback(const velodyne_msgs::msg::VelodyneScan::SharedPtr msg)
-  {
-    velodyne_points_sub_->publish(*msg);
-  }
-  void TopicNameChanger::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg)
-  {
-    imu_sub_->publish(*msg);
-  }
-  void TopicNameChanger::gnss_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg)
-  {
-    gnss_sub_->publish(*msg);
-  }
+void TopicNameChanger::velodyne_points_callback(const velodyne_msgs::msg::VelodynePacket::SharedPtr msg) {
+  velodyne_points_pub_->publish(*msg);
+}
+void TopicNameChanger::imu_callback(const sensor_msgs::msg::Imu::SharedPtr msg) {
+  imu_pub_->publish(*msg);
+}
+void TopicNameChanger::gnss_callback(const sensor_msgs::msg::NavSatFix::SharedPtr msg) {
+  gnss_pub_->publish(*msg);
+}
